6b43d4f3aa37fe774d12328d111dd269ab69c63c
jpofficials7 <jpofficials7@gmail.com>
2024-05-20 15:49:28 +0530

    jokes generator

---
+++ src/components/Joke.jsx
@@ -0,0 +1,23 @@
+import { React, useState } from 'react';
+
+import Button from './Button';
+import './Joke.css';
+
+const Joke = () => {
+  const [joke, setJoke] = useState('');
+
+  const fetchApi = () => {
+    fetch('https://sv443.net/jokeapi/v2/joke/Programming?type=single')
+      .then((res) => res.json())
+      .then((data) => setJoke(data.joke));
+  };
+
+  return (
+    <div className="Joke">
+      <Button callApi={fetchApi} />
+      <p>{joke}</p>
+    </div>
+  );
+};
+
+export default Joke;
